85063a975e982d48be78c6435d437663154eba48
Tyler Schum <tschumdev@gmail.com>
2020-03-11 19:14:50 -0400

    add global stylings to app.css and import using overwritten app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from "next/app";
+import "../app.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
-export default () => <h1>Hello World</h1>;
+const FancyDiv = styled.div`
+  height: 400px;
+  width: 600px;
+  background-color: #666;
+  color: #eee;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 0.2s linear;
+
+  &:hover {
+    background-color: tomato;
+  }
+`;
+
+export default () => (
+  <FancyDiv>
+    <h1>Hello World</h1>
+  </FancyDiv>
+);
