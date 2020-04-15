f1035a531d457f421cae7930eb58f326d369d68a
Tyler Schum <tschumdev@gmail.com>
2020-04-15 16:18:56 -0400

    fix typo in logo url

--- components/metadata.tsx
+++ components/metadata.tsx
@@ -9,6 +9,7 @@ const DESCRIPTION =
   "At Developer Deep Dives working professionals, hobbyists, and beginners come together to explore new and important technologies in the software development world. Along with learning, we have built a network of skilled developers that is able to benefit all of our attendees with any problem they may encounter in their career. We meet at The Ergonomic Group, Inc in Westbury, our corporate sponsor who provides us a space to meet in, as well as other benefits.";
 const TITLE = "Developer Deep Dives";
 const UPDATED_AT = new Date(Date.now()).toISOString();
+const IMAGE_URL = "/ddd_logo.jpg";
 
 const Metadata: React.FC<Props> = ({ pageName }) => {
   return (
@@ -21,8 +22,8 @@ const Metadata: React.FC<Props> = ({ pageName }) => {
       <meta name="og:url" content="https://www.developerdeepdives.com/" />
       <meta name="og:site_name" content="Developer Deep Dives" />
       <meta name="og:updated_time" content={UPDATED_AT} />
-      <meta name="og:image" content="/ddd_logo.jpeg" />
-      <meta name="og:image:secure_url" content="/ddd_logo.jpeg" />
+      <meta name="og:image" content={IMAGE_URL} />
+      <meta name="og:image:secure_url" content={IMAGE_URL} />
       <meta name="og:image:width" content="300" />
       <meta name="og:image:height" content="168" />
       <meta name="og:image:alt" content="Homepage" />
